280255947a7d0d9ce6086955cae860c92494e167
zanuzzorz <zanuzzorz@gmail.com>
2016-08-15 17:36:41 -0300

    update chatWeb2

--- chatWeb2/chatWeb.py
+++ chatWeb2/chatWeb.py
@@ -3,6 +3,7 @@ import requests, bottle, json, threading, time, sys
 
 
 peers = sys.argv[2:]
+
 messages = []
 
 @get('/')
@@ -31,28 +32,36 @@ def index():
 def index():
 	return json.dumps(messages)
 
-def client():
+def client_peers():
 	time.sleep(5)
 	while True:
 		np = []
-		nm = []
 		for p in peers:
 			r = requests.get(p + '/peers')
 			np = np + json.loads(r.text)
-
-			m = requests.get(p + '/messages')
-			nms = json.loads(m.text)
-			
-			for msg in nms:
-					if msg not in messages:
-						messages.append(msg)
-	
 		peers[:] = list(set(np + peers))
 		print(peers)
 		time.sleep(1)
 
-t = threading.Thread(target=client)
+
+def client_messages():
+	time.sleep(5)
+	while True:
+		nm = []
+		for p in peers:
+			m = requests.get(p + '/messages')
+			nms = json.loads(m.text)
+			for msg in nms:
+					if msg not in messages:
+						messages.append(msg)
+	
+		time.sleep(1)
+
+t = threading.Thread(target=client_peers)
 t.start()
+
+t2 = threading.Thread(target=client_messages)
+t2.start()
 
 
 run(host='localhost', port=int(sys.argv[1]))
